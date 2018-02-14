1f7522f2e7cfb87024534158287d0a33b19800a9
deni2312 <denisbeqi2312@gmail.com>
2018-02-14 20:41:07 +0100

    Update Pointer_mouse_hand_move.cpp

--- Pointer_mouse_hand_move.cpp
+++ Pointer_mouse_hand_move.cpp
@@ -22,62 +22,59 @@
  /** @function main */
  int main()
  {
-	 //initializing functions
-   HideConsole();
-   GetDesktopResolution(x, y);
-   Mat frame;
+   	Mat frame;
+	VideoCapture cap;
+	//initializing functions
+	HideConsole();
+   	GetDesktopResolution(x, y);
    //-- 1. Load the cascades
-   if( !cascade.load( cascade_name ) )
-   { 
-        printf("--(!)Error loading\n"); return -1; 
-   }
-   VideoCapture cap;
+	if( !cascade.load( cascade_name ) )
+    { 
+		printf("--(!)Error loading\n"); return -1; 
+    }
    // open the default camera, use something different from 0 otherwise;
    // Check VideoCapture documentation.
-   if ( !cap.open(0) )
-   {
-	 return 0;
-   }
-     while( cap.read(frame) )
-     {
-   //-- 3. Apply the classifier to the frame
-       if( !frame.empty() )
-       { 
-	   detectAndDisplay( frame ); 
-       }else
-	   { 
-	   	printf(" --(!) No captured frame -- Break!"); 
+	if ( !cap.open(0) )
+    {
+		return 0;
+    }
+	while( cap.read(frame) )
+    {
+   		//-- 3. Apply the classifier to the frame
+       	if( !frame.empty() )
+       	{ 
+	   		detectAndDisplay( frame ); 
+       	}
+		else
+	   	{ 
+	   		printf(" --(!) No captured frame -- Break!"); 
 	        break; 
-	   }
-
-       int c = waitKey(10);
-       if( (char)c == 'c' ) 
-       {
-	    break; 
-       }
-      }
-   return 0;
+	   	}
+       	int c = waitKey(10);
+       	if( (char)c == 'c' ) 
+       	{
+	    	break; 
+       	}
+   	}
+   	return 0;
  }
 
 /** @function detectAndDisplay */
 void detectAndDisplay( Mat frame )
 {
-  std::vector<Rect> hands;
-  Mat frame_gray;
-
-  cvtColor( frame, frame_gray, CV_BGR2GRAY );
-  equalizeHist( frame_gray, frame_gray );
-
-  //-- Detect hands
-  cascade.detectMultiScale( frame_gray, hands, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(100, 100), Size(160, 160));
-
-  for( size_t i = 0; i < hands.size(); i++ )
-  {
-	//this function sets the cursor position
-	SetCursorPos(x-(x/frame.cols+2)*hands[i].x,(y/(frame.rows-220))*hands[i].y);
-  }
-  //-- Show what you got
-  //imshow( window_name, frame );
+	std::vector<Rect> hands;
+  	Mat frame_gray;
+  	cvtColor( frame, frame_gray, CV_BGR2GRAY );
+  	equalizeHist( frame_gray, frame_gray );
+  	//-- Detect hands
+  	cascade.detectMultiScale( frame_gray, hands, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(100, 100), Size(160, 160));
+  	for( size_t i = 0; i < hands.size(); i++ )
+  	{
+		//this function sets the cursor position
+		SetCursorPos(x-(x/frame.cols+2)*hands[i].x,(y/(frame.rows-220))*hands[i].y);
+  	}
+  	//-- Show what you got
+  	//imshow( window_name, frame );
  }
 
 /** @function GetDesktopResolution */
